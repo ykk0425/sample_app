48d0a93e6b5f7b16d865fb853525ae034cf1f5cb
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-01-06 14:39:06 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,13 +6,14 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
   def index
      @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
