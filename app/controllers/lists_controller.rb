1539aa20855611011b561990f17948f6f5e17e68
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-01-07 14:56:40 +0000

    [Finish]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,6 +19,12 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end  
   
   private
   def list_params
