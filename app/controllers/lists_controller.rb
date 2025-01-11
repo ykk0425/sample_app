68206158340e5f6f385737d3833f54a2e78a99e3
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-01-11 12:29:22 +0000

    [Finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,10 +4,14 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+       redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
+
   def index
      @lists = List.all
   end
